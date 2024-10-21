1d3e7eeb0f78285743107f98d012c182462a9784
WawinyEdwin <wawinyedwin44@gmail.com>
2024-10-21 22:39:05 +0300

    use third party ga component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
-import GoogleAnalytics from "./google-analytics";
 
 export default function RootLayout({
   children,
@@ -18,8 +18,8 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+      <GoogleAnalytics gaId="G-9PMXVY26RC" />
       <head />
-      <GoogleAnalytics />
       <body>
         <Providers>
           <Header />
